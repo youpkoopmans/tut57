9973ea12535387908d6ffb8228871703c56341e5
Youp Koopmans <youpkoopmans@hotmail.com>
2018-10-11 19:45:28 +0200

    Revert "Test"
    This reverts commit 98b12f65cdeb8c144c55b45aab58defe108bfc1a.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Youp Koopmans
+                    Laravel
                 </div>
 
                 <div class="links">

98b12f65cdeb8c144c55b45aab58defe108bfc1a
Youp Koopmans <youpkoopmans@hotmail.com>
2018-10-11 19:39:13 +0200

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Youp Koopmans
                 </div>
 
                 <div class="links">
